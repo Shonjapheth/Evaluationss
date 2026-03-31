189e5f136268a16b2086b6bee5514badb8de2d8b
unknown <you@example.com>
2026-03-31 15:18:38 +0800

    Updated

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 try {
     $conn = new PDO(
-        "pgsql:host=YOUR_HOST;port=5432;dbname=evaluation_db_m4cd",
+        "pgsql:host=dpg-d75mltffte5s73e6m2kg-a.oregon-postgres.render.com;port=5432;dbname=evaluation_db_m4cd",
         "evaluation_db_m4cd_user",
         "JNpe5tSt0jcPcCri0IyCnZeOB1CDZDKC"
     );
